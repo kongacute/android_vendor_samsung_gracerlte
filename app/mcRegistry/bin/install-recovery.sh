c44a9e599c9f3b96146f866c057c37423ab2a148
riceplay33 <48028392+riceplay33@users.noreply.github.com>
2019-02-26 19:19:14 +0100

    Add files via upload

---
+++ app/mcRegistry/bin/install-recovery.sh
@@ -0,0 +1,46 @@
+#!/system/bin/sh
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:46483744:866056d6babe6acd31ce7886f2cb796000babab1; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:38856992:8430dc6a59fda37386c3f9fb2bd5c193a6351add EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 866056d6babe6acd31ce7886f2cb796000babab1 46483744 8430dc6a59fda37386c3f9fb2bd5c193a6351add:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || echo 454 > /cache/fota/fota.status
+else
+  log -t recovery "Recovery image already installed"
+fi
+
+if [ -e /cache/recovery/command ] ; then
+  PACKAGE_PATH=""
+  SEARCH_COMMAND="--update_package"
+  PATH_POS=16
+  if [ -e '/system/bin/grep' ] ; then
+    PACKAGE_PATH=`cat /cache/recovery/command | grep 'update_package'`
+    PACKAGE_ORG_PATH=`cat /cache/recovery/command | grep 'update_org_package'`
+    if [ "$PACKAGE_ORG_PATH" != "" ] ; then
+      PACKAGE_PATH=$PACKAGE_ORG_PATH
+      SEARCH_COMMAND="--update_org_package"
+      PATH_POS=20
+    fi
+    if [ -e /cache/recovery/saved" ] ; then
+      rm -rf /cache/recovery/saved
+    fi
+
+    if [ -e /data/.recovery/saved" ] ; then
+      rm -rf /data/.recovery/saved
+    fi
+  fi
+  if [ "$PACKAGE_PATH" != "" ] ; then
+    for PACKAGE_LINE in $PACKAGE_PATH
+    do
+      if [ ${PACKAGE_LINE:0:$PATH_POS} == $SEARCH_COMMAND ] ; then
+        break
+      fi
+    done
+    let PATH_POS+=1
+    PACKAGE_PATH=${PACKAGE_LINE:$PATH_POS}
+    if [ "$PACKAGE_PATH" != "" ] ; then
+      rm $PACKAGE_PATH
+    fi
+  fi
+  if [ ${PACKAGE_PATH:0:5} == "/data" ] ; then
+    echo $PACKAGE_PATH > /cache/fota/fota_path_command
+    chown system:system /cache/fota/fota_path_command
+  fi
+  rm /cache/recovery/command
+fi
